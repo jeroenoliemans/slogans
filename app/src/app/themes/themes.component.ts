25e0d1796487ce70634d2f8074b363f813836955
Jeroen Oliemans <jeroen.oliemans@avinty.com>
2024-03-24 17:56:15 +0100

    form

--- app/src/app/themes/themes.component.ts
+++ app/src/app/themes/themes.component.ts
@@ -2,20 +2,44 @@ import { Component } from '@angular/core';
 import {Store}  from '../store/Store'
 import {ThemeService} from '../service/themes.service'
 import {NgForOf} from "@angular/common";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, FormBuilder, ReactiveFormsModule} from "@angular/forms";
 import {SloganComponent} from "../slogan/slogan.component";
 
 @Component({
   selector: 'app-themes',
   standalone: true,
-  imports: [NgForOf, ReactiveFormsModule, SloganComponent],
+  imports: [FormsModule, NgForOf, ReactiveFormsModule, SloganComponent],
   templateUrl: './themes.component.html',
   styleUrl: './themes.component.css'
 })
 export class ThemesComponent {
+  themeForm = this.formBuilder.group({
+    label: '',
+    backgroundColor: '',
+    backgroundColorLeft: '',
+    backgroundColorRight: '',
+    borderColor: '',
+    fontColor: ''
+  })
+
   constructor(
     public themeService: ThemeService,
+    private formBuilder: FormBuilder,
     public store: Store
   ) {
+  }
+
+  onSubmit() {
+    this.themeService.addTheme({
+      id: null,
+      label: this.themeForm.value.label as string,
+      backgroundColor: this.themeForm.value.backgroundColor as string,
+      backgroundColorLeft: '',
+      backgroundColorRight: '',
+      borderColor: '',
+      fontColor: ''
+    })
+    this.themeForm.reset();
+    this.themeService.fetchThemes()
   }
 }
